45546486d331e9d575ec910acef324cd96412ec7
Armando Silveira <armando@armandos-air.hsd1.ga.comcast.net>
2017-03-22 15:57:12 -0400

    Initial commit

--- Guess The Number/ViewController.swift
+++ Guess The Number/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var guessTextField: UITextField!
+
+    
+    let lowerBound = 1
+    let upperBound = 100
+    var numberToGuess = 0 //: Int! // what, how does this work
+    var numberOfGuesses = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        generateRandomNumber()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,56 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func generateRandomNumber() {
+        // arc4random_uniform(100) will generate numbers from 0 to 99
+        // which is 100 numbers, but we want 1 from to 100
+        numberToGuess = Int(arc4random_uniform(100)) + 1
+        
+    }
 
+    @IBAction func submitButtonPressed(_ sender: Any) {
+        if let guess = guessTextField.text {
+            if let guessInt = Int(guess) {
+                numberOfGuesses = numberOfGuesses + 1
+                validateGuess(guessInt)
+            }
+        }
+        
+    }
+    
+    func validateGuess(_ guess: Int) {
+        if guess < lowerBound || guess > upperBound {
+            print("Your guess should be between 1 and 100!")
+            showBoundsAlert()
+        } else if guess < numberToGuess {
+            //print("Higher!")
+            textLabel.text = "Higher! ⬆️"
+        } else if guess > numberToGuess {
+            //print("Lower!")
+            textLabel.text = "Lower! ⬇️"
+        } else {
+            print("You win!")
+            showWinAlert()
+            textLabel.text = "Guess The Number"
+            numberOfGuesses = 0
+            generateRandomNumber()
+        }
+        guessTextField.text = ""
+    }
+    
+    func showBoundsAlert() {
+        let alert = UIAlertController(title: "Hey!", message: "Your guess should be between 1 and 100", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Got it", style: .default, handler: nil)
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func showWinAlert() {
+        let alert = UIAlertController(title: "Congrats! 🎉", message: "You won with \(numberOfGuesses) guesses", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Play Again", style: .default, handler: nil)
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+        
+    }
 }
 
